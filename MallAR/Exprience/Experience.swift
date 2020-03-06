fc1c34e39e1e905273b7a807e40b83b0e0878212
Amirhossein Pooya <amirhosseinpooya@gmail.com>
2020-03-06 22:18:19 +0330

    change app to be suitable for image recognition

--- MallAR/Exprience/Experience.swift
+++ MallAR/Exprience/Experience.swift
@@ -43,6 +43,26 @@ enum Experience {
         cancellable?.store(in: &streams)
     }
     
+    public static func loadSilverSceneAsync(completion: @escaping (Swift.Result<Experience.Scene, Swift.Error>) -> Void) {
+        guard let realityFileURL = Foundation.Bundle(for: Experience.Scene.self).url(forResource: "Coin", withExtension: "reality") else {
+            completion(.failure(Experience.LoadRealityFileError.fileNotFound("Coin.reality")))
+            return
+        }
+
+        var cancellable: Combine.AnyCancellable?
+        let realityFileSceneURL = realityFileURL.appendingPathComponent("SilverCoinScene", isDirectory: false)
+        let loadRequest = Experience.Scene.loadAnchorAsync(contentsOf: realityFileSceneURL)
+        cancellable = loadRequest.sink(receiveCompletion: { loadCompletion in
+            if case let .failure(error) = loadCompletion {
+                completion(.failure(error))
+            }
+            streams.removeAll { $0 === cancellable }
+        }, receiveValue: { entity in
+            completion(.success(Experience.createScene(from: entity)))
+        })
+        cancellable?.store(in: &streams)
+    }
+    
     public enum LoadRealityFileError: Error {
           case fileNotFound(String)
     }
@@ -55,21 +75,19 @@ enum Experience {
     }
     
     public class Scene: RealityKit.Entity, RealityKit.HasAnchoring {
-        var goldCoin: RealityKit.Entity? {
-            return self.findEntity(named: "gold_coin")
-        }
+//        var goldCoin: RealityKit.Entity? {
+//            return self.findEntity(named: "SilverCoinScene")
+//        }
     }
 }
 
 extension Experience {
     public struct AnchorPlacement {
-
         /// The identifier of the anchor the game is placed on. Used to re-localized the game between levels.
         var arAnchorIdentifier: UUID?
 
         /// The transform of the anchor the game is placed on . Used to re-localize the game between levels.
         var placementTransform: Transform?
-
     }
 }
 
--- MallAR/ViewController.swift
+++ MallAR/ViewController.swift
@@ -22,27 +22,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewDidLoad()
         
         self.setupView()
-        self.configureARView()
-        self.addCoachingView()
-        self.presentCoachingOverlay()
-        self.transitionToAppStart()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-//        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-//        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-//        sceneView.session.pause()
+//        self.configureARView()
+//        self.addCoachingView()
+//        self.presentCoachingOverlay()
+//        self.transitionToAppStart()
     }
     
     func setupView() {
@@ -53,127 +36,134 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    func configureARView() {
-        let arConfiguration = ARWorldTrackingConfiguration()
-        arConfiguration.planeDetection = .horizontal
-        arView.session.run(arConfiguration)
-    }
-    
-    func addCoachingView() {
-        self.coachingOverlay = ARCoachingOverlayView()
-        self.arView.addSubview(self.coachingOverlay)
-        self.coachingOverlay.snp.makeConstraints { make in
-            make.bottom.top.leading.trailing.equalToSuperview()
-        }
-//        NetworkConnection.shared.send(apiRequest: APIRouter.getContacts).subscribeOn()
-    }
-    
-    func presentCoachingOverlay() {
-        coachingOverlay.session = arView.session
-        coachingOverlay.delegate = self
-        coachingOverlay.goal = .horizontalPlane
-        coachingOverlay.activatesAutomatically = false
-        self.coachingOverlay.setActive(true, animated: true)
-    }
-    
-    func transitionToAppStart() {
-        Experience.loadSceneAsync { [weak self] result in
-            switch result {
-            case .success(let game):
-                guard let self = self else { return }
-                
-                if self.sceneAnchor == nil {
-                    self.sceneAnchor = game
-//                    self.observer?.gameControllerContentDidLoad(self)
-                }
-                
-//                if case let .waitingForContent(nextState) = self.currentState {
-//                    self.transition(to: nextState)
-//                }
-            case .failure(let error):
-                print("Unable to load the game with error: \(error.localizedDescription)")
-            }
-        }
+    func addConinsToView() {
         
-//        transition(to: .menu)
     }
     
-    func addAnchorToARView() {
-        guard let scene = self.sceneAnchor else { return }
-        UIApplication.shared.isIdleTimerDisabled = true
-        self.arView.scene.addAnchor(scene)
-    }
+//    func configureARView() {
+//        let arConfiguration = ARWorldTrackingConfiguration()
+//        arConfiguration.planeDetection = .vertical
+//        arConfiguration.environmentTexturing = .automatic
+//        arView.session.run(arConfiguration)
+//    }
     
+//    func addCoachingView() {
+//        self.coachingOverlay = ARCoachingOverlayView()
+//        self.arView.addSubview(self.coachingOverlay)
+//        self.coachingOverlay.snp.makeConstraints { make in
+//            make.bottom.top.leading.trailing.equalToSuperview()
+//        }
+//    }
+    
+//    func presentCoachingOverlay() {
+//        coachingOverlay.session = arView.session
+//        coachingOverlay.delegate = self
+//        coachingOverlay.goal = .horizontalPlane
+//        coachingOverlay.activatesAutomatically = false
+//        self.coachingOverlay.setActive(true, animated: true)
+//    }
+    
+//    func transitionToAppStart() {
+//        Experience.loadSilverSceneAsync { [weak self] result in
+//            switch result {
+//            case .success(let game):
+//                guard let self = self else { return }
+//
+//                if self.sceneAnchor == nil {
+//                    self.sceneAnchor = game
+//                    self.addAnchorToARView()
+////                    self.observer?.gameControllerContentDidLoad(self)
+//                }
+//            case .failure(let error):
+//                print("Unable to load the game with error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+    
+//    func addAnchorToARView() {
+//        guard let scene = self.sceneAnchor else { return }
+//        UIApplication.shared.isIdleTimerDisabled = true
+////        self.arView.scene.addAnchor(scene)
+//        self.arView.scene.anchors.append(scene)
+//    }
 }
 
-extension ViewController: ARCoachingOverlayViewDelegate {
-    func coachingOverlayViewWillActivate(_ coachingOverlayView: ARCoachingOverlayView) {
-        // Ask the user to gather more data before placing the game into the scene
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            // Set the view controller as the delegate of the session to get updates per-frame
-            self.arView.session.delegate = self
-        }
-    }
-}
+//extension ViewController: ARCoachingOverlayViewDelegate {
+//    func coachingOverlayViewWillActivate(_ coachingOverlayView: ARCoachingOverlayView) {
+//        // Ask the user to gather more data before placing the game into the scene
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+//            // Set the view controller as the delegate of the session to get updates per-frame
+//            self.arView.session.delegate = self
+//        }
+//    }
+//}
 
 /// Used to find a horizontal plane anchor before placing the game into the world.
-extension ViewController: ARSessionDelegate {
-    func session(_ session: ARSession, didUpdate frame: ARFrame) {
-        let screenCenter = CGPoint(x: arView.frame.midX, y: arView.frame.midY)
-
-        let results = arView.hitTest(screenCenter, types: [.existingPlaneUsingExtent])
-        guard let result = results.first(where: { result -> Bool in
-
-            // Ignore results that are too close or too far away to the camera when initially placing
-            guard result.distance > 0.5 && result.distance < 2.0 || self.coachingOverlay.isActive else {
-                return false
-            }
-
-            // Make sure the anchor is a horizontal plane with a reasonable extent
-            guard let planeAnchor = result.anchor as? ARPlaneAnchor,
-                planeAnchor.alignment == .horizontal else {
-                    return false
-            }
-
-            // Make sure the horizontal plane has a reasonable extent
-            let extent = simd_length(planeAnchor.extent)
-            guard extent > 1 && extent < 2 else {
-                return false
-            }
-
-            return true
-        }),
-        let planeAnchor = result.anchor as? ARPlaneAnchor else {
-            return
-        }
-
-        // Create an anchor and add it to the session to place the game at this location
-        let gameAnchor = ARAnchor(name: "Game Anchor", transform: normalize(planeAnchor.transform))
-        arView.session.add(anchor: gameAnchor)
-
-        self.anchorPlacement = Experience.AnchorPlacement(arAnchorIdentifier: gameAnchor.identifier,
-                                                                    placementTransform: Transform(matrix: planeAnchor.transform))
-
-        // Remove the coaching overlay view
-        self.coachingOverlay.delegate = nil
-        self.coachingOverlay.setActive(false, animated: false)
-        self.coachingOverlay.removeFromSuperview()
-
-        // Now that an anchor has been found, remove the view controller as a delegate to stop receiving updates per-frame
-        arView.session.delegate = nil
-
-        // Reset the session to stop searching for horizontal planes after we found the anchor for the game
-        arView.session.run(ARWorldTrackingConfiguration())
-        
-        self.addAnchorToARView()
-//        self.gameController.playerReadyToBowlFrame()
-    }
-
-    func normalize(_ matrix: float4x4) -> float4x4 {
-        var normalized = matrix
-        normalized.columns.0 = simd.normalize(normalized.columns.0)
-        normalized.columns.1 = simd.normalize(normalized.columns.1)
-        normalized.columns.2 = simd.normalize(normalized.columns.2)
-        return normalized
-    }
-}
+//extension ViewController: ARSessionDelegate {
+//
+//
+//    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+//        let screenCenter = CGPoint(x: arView.frame.midX, y: arView.frame.midY)
+//
+//        let results = arView.hitTest(screenCenter, types: [.existingPlaneUsingExtent])
+//        guard let result = results.first(where: { result -> Bool in
+//
+//            // Ignore results that are too close or too far away to the camera when initially placing
+//            guard result.distance > 0.5 /* && result.distance < 1.0 */ || self.coachingOverlay.isActive else {
+//                return false
+//            }
+//
+//            // Make sure the anchor is a horizontal plane with a reasonable extent
+//            guard let planeAnchor = result.anchor as? ARPlaneAnchor,
+//                planeAnchor.alignment == .horizontal else {
+//                    return false
+//            }
+//
+//            // Make sure the horizontal plane has a reasonable extent
+//            let extent = simd_length(planeAnchor.extent)
+//            guard extent > 0.2 /*&& extent < 2 */ else {
+//                return false
+//            }
+//
+//            return true
+//        }),
+//        let planeAnchor = result.anchor as? ARPlaneAnchor else {
+//            return
+//        }
+//
+//        // Create an anchor and add it to the session to place the game at this location
+//        let gameAnchor = ARAnchor(name: "Game Anchor", transform: normalize(planeAnchor.transform))
+//        arView.session.add(anchor: gameAnchor)
+//
+//        self.anchorPlacement = Experience.AnchorPlacement(arAnchorIdentifier: gameAnchor.identifier,
+//                                                                    placementTransform: Transform(matrix: planeAnchor.transform))
+//
+//        // Remove the coaching overlay view
+//        self.coachingOverlay.delegate = nil
+//        self.coachingOverlay.setActive(false, animated: false)
+//        self.coachingOverlay.removeFromSuperview()
+//
+//        // Now that an anchor has been found, remove the view controller as a delegate to stop receiving updates per-frame
+//        arView.session.delegate = nil
+//
+//        // Reset the session to stop searching for horizontal planes after we found the anchor for the game
+//
+//        let config = ARWorldTrackingConfiguration()
+//        if ARWorldTrackingConfiguration.supportsFrameSemantics(.personSegmentationWithDepth) {
+//            config.frameSemantics.insert(.personSegmentationWithDepth)
+//        }
+//
+//        arView.session.run(config)
+//
+//        self.addAnchorToARView()
+////        self.gameController.playerReadyToBowlFrame()
+//    }
+//
+//    func normalize(_ matrix: float4x4) -> float4x4 {
+//        var normalized = matrix
+//        normalized.columns.0 = simd.normalize(normalized.columns.0)
+//        normalized.columns.1 = simd.normalize(normalized.columns.1)
+//        normalized.columns.2 = simd.normalize(normalized.columns.2)
+//        return normalized
+//    }
+//}
